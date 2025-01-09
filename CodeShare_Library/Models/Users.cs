bfd245c303c80dea53fdbe2ed96d02c0de3f68b1
Maks <bobreczovm@inbox.ru>
2025-01-09 14:45:37 +0300

    Добавьте файлы проекта.

---
+++ CodeShare_Library/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeShare_Library.Models
+{
+    public class Users
+    {
+        public long  Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Date_registration {  get; set; }
+
+
+    }
+}
