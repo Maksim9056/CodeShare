819904bbc21cbc8470fba578d770b781f48df2dc
Maks <bobreczovm@inbox.ru>
2025-01-31 11:03:57 +0300

    Правки

--- CodeShare_Library/Service/LanguageServise.cs
+++ CodeShare_Library/Service/LanguageServise.cs
@@ -13,12 +13,12 @@ namespace CodeShare_Library.Service
     public class LanguageServise : ILanguage
     {
         public readonly string UrlRolesApi;
-        public string url;
-        public readonly HttpClient _httpClient; // HttpClient для отправки запросов
+        public string url ="";
+        private readonly HttpClient _httpClient = new HttpClient();
         public LanguageServise(string urlRolesApi)
         {
             UrlRolesApi = urlRolesApi;
-           _httpClient = new HttpClient();
+           //_httpClient = new HttpClient();
 
         }
 
@@ -46,13 +46,30 @@ namespace CodeShare_Library.Service
 
         public async Task<List<Language>> GetAllAsync()
         {
+            try
+            {
 
-            url = $"{UrlRolesApi}/Language/language/all";
-            _httpClient.BaseAddress = new Uri(url);
-            List<Language> Roles_responseMessage = await _httpClient.GetFromJsonAsync< List<Language>>(url);
+                HttpClient httpClient = new HttpClient();
 
-            return Roles_responseMessage;
+                url = $"{UrlRolesApi}/Language/language/all";
+                if (httpClient.BaseAddress == null)
+                {
+                    httpClient.BaseAddress = new Uri(url);
 
+                }
+                else
+                {
+                    httpClient.BaseAddress = new Uri(url);
+
+                }
+                List<Language> Roles_responseMessage = await httpClient.GetFromJsonAsync<List<Language>>(url);
+                //_httpClient.BaseAddress = null;
+                return Roles_responseMessage;
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new List<Language>();
+            }
         }
 
         public async Task<Language> GetByIdAsync(long id)
